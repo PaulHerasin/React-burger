8170e401cca3c578b14038fea9bcb32df1aa7177
pavel <geraci98.98@gmail.com>
2020-02-03 19:40:56 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+import Layout from "./components/Layout/Layout";
+import BurgerBulder from "./containers/BurgerBuilder/BurgerBuilder";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <BurgerBulder></BurgerBulder>
+        </Layout>
+      </div>
+    );
+  }
+}
+
+export default App;
